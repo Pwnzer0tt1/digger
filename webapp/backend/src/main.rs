a4ba32d49cea9c494371b8271c2ebb129fe1a231
Oscar Urselli <urselli.oscar@gmail.com>
2025-09-04 01:14:22 +0200

    core(webapp): [actix-web-migration] Setup Diesel ORM and crate CTF config struct.

--- webapp/backend/src/main.rs
+++ webapp/backend/src/main.rs
@@ -1,11 +1,27 @@
+use std::sync::{Arc, Mutex};
+
 use actix_web::{get, post, delete, web, App, HttpResponse, HttpServer, Responder};
+use backend::config::CtfConfig;
+use diesel::{r2d2::{self, ConnectionManager}, PgConnection};
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
+    dotenvy::dotenv().ok();
+
+    // Create Postgres connection
+    let database_url = std::env::var("DATABASE_URL").unwrap_or(String::from("postgresql://postgres@postgres:5432/postgres"));
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder().build(manager).expect("Failed to create database connection pool.");
+    
+    // Init CTF configuration and load existing JSON
+    let mut ctf_config= CtfConfig::new();
+    ctf_config.load("./ctf_config.json").expect("IO error while loading CTF configuration.");
+
+    let static_files = String::from("../frontend/build");
+
+    let server = HttpServer::new(move || {
+        let app = App::new()
             .service(get_config)
             .service(set_config)
             .service(set_services)
@@ -17,15 +33,30 @@ async fn main() -> std::io::Result<()> {
             .service(get_flow_raw)
             .service(get_stats)
             .service(get_stats_suricata)
+            .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(Arc::new(Mutex::new(ctf_config.clone()))));
+
+        if cfg!(not(debug_assertions)) {
+            return app.service(
+                actix_files::Files::new("/", static_files.clone())
+                    .index_file("index.html")
+                    .default_handler(
+                        actix_files::NamedFile::open(
+                            vec![static_files.clone(), "index.html".to_string()].join("/")
+                        )
+                        .expect("index.html not found.")
+                    )
+            );
+        }
+
+        app
     })
     .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
+    .run();
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello worlddddd!")
+    println!("Server is running at http://127.0.0.1:8080/");
+
+    server.await
 }
 
 #[get("/api/config")]
